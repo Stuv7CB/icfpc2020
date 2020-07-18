79ffbdff5d28e8062af6ec9adeeab9c713a7d629
Gravechapa <Gravechapa@protonmail.com>
2020-07-18 04:40:32 +0300

    Implement basic connection to alien relay

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,47 @@
 #include <httplib.h>
 #include "Modulation.hpp"
 
+void printResponse(std::vector<std::any> resp)
+{
+    std::cout << '(';
+    for (auto &el : resp)
+    {
+        if (typeid(BigInt) == el.type())
+        {
+            std::cout << std::any_cast<BigInt>(el);
+        }
+        if (typeid(std::vector<std::any>) == el.type())
+        {
+            printResponse(std::any_cast<std::vector<std::any>>(el));
+        }
+        std::cout << ", ";
+    }
+    std::cout << ')';
+}
+
+void errorCheck(std::shared_ptr<httplib::Response> &serverResponse)
+{
+    if (!serverResponse)
+    {
+        std::cout << "Unexpected server response:\nNo response from server" << std::endl;
+        exit(1);
+    }
+
+    if (serverResponse->status != 200)
+    {
+        std::cout << "Unexpected server response:\nHTTP code: " << serverResponse->status
+                  << "\nResponse body: " << serverResponse->body << std::endl;
+        exit(2);
+    }
+}
+
 int main(int argc, char* argv[])
 {
     std::string_view test = "110110000111011111100001001110101100111000";
-    auto dl = Modulation::demodulateList(test);
+    printResponse(Modulation::demodulateList(test));
+    std::cout << std::endl;
+
+    std::shared_ptr<httplib::Response> serverResponse;
     if (argc == 3)
     {
         const std::string serverUrl(argv[1]);
@@ -25,27 +62,26 @@ int main(int argc, char* argv[])
         const std::string serverName = urlMatches[1];
         const int serverPort = std::stoi(urlMatches[2]);
         httplib::Client client(serverName, serverPort);
-        const std::shared_ptr<httplib::Response> serverResponse =
-            client.Post(serverUrl.c_str(), playerKey.c_str(), "text/plain");
-
-        if (!serverResponse)
-        {
-            std::cout << "Unexpected server response:\nNo response from server" << std::endl;
-            return 1;
-        }
-
-        if (serverResponse->status != 200)
-        {
-            std::cout << "Unexpected server response:\nHTTP code: " << serverResponse->status
-                      << "\nResponse body: " << serverResponse->body << std::endl;
-            return 2;
-        }
-
-        std::cout << "Server response: " << serverResponse->body << std::endl;
+        serverResponse = client.Post(serverUrl.c_str(), playerKey.c_str(), "text/plain");
+        errorCheck(serverResponse);
     }
     else
     {
-
+        httplib::SSLClient client("icfpc2020-api.testkontur.ru");
+        serverResponse =  client.Post("/aliens/send?apiKey=42d71c651c4a4048842b8973c8bb3608", "0", "text/plain");
+        errorCheck(serverResponse);
+        test = serverResponse->body;
+        printResponse(Modulation::demodulateList(test));
+        std::cout << std::endl;
+        std::cout << "Server response: " << serverResponse->body << std::endl;
+        serverResponse =  client.Post("/aliens/send?apiKey=42d71c651c4a4048842b8973c8bb3608",
+                                      serverResponse->body, "text/plain");
+        errorCheck(serverResponse);
+        test = serverResponse->body;
+        printResponse(Modulation::demodulateList(test));
+        std::cout << std::endl;
     }
+
+    std::cout << "Server response: " << serverResponse->body << std::endl;
     return 0;
 }
